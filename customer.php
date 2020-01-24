567e44fa3ea7bd7cd95aba036b4ea38371137ee8
milivojevic85 <misagrf@yahoo.com>
2020-01-24 18:31:39 +0100

    constructor simple output

--- customer.php
+++ customer.php
@@ -8,27 +8,22 @@
 <?php 
 class Customer
 {
-	private $id = 1;
+	private $id;
 	public $name;
 	public $email;
 	public $balance;
 	
-	public function __construct() {
-		echo "The constructor ran...";
-	}
-	
-	public function getCustomer($id) {
+	public function __construct($id, $name, $email, $balance) {
 		$this->id = $id;
-		return "John Doe";
+		$this->name = $name;
+		$this->email = $email;
+		$this->balance = $balance;
 	}
-	
-	public function __destruct() {
-		echo "The destructor ran...";
-	}
+
 }
 
-$customer = new Customer;
-echo $customer->getCustomer(10); // The constructor ran...John Doe The destructor ran...
+$customer = new Customer(1, "Brad Traversy","brad@gmail.com",0);
+echo $customer->name; // Brad Traversy
 
 
 ?>
