91386c083b955edb3fb0cdf19c477ed3eefe1b91
milivojevic85 <misagrf@yahoo.com>
2020-01-24 18:16:07 +0100

    first steps

--- customer.php
+++ customer.php
@@ -6,7 +6,21 @@
 </head>
 <body>
 <?php 
+class Customer
+{
+	private $id = 1;
+	public $name;
+	public $email;
+	public $balance;
+	
+	public function getCustomer($id) {
+		$this->id = $id;
+		return "John Doe";
+	}
+}
 
+$customer = new Customer;
+echo $customer->getCustomer(1);
 
 ?>
 </body>
