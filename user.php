db81f2024bdee60ceb759099d476bb85f567a42e
milivojevic85 <misagrf@yahoo.com>
2020-01-24 19:40:45 +0100

    calling static property in method, and method by class

--- user.php
+++ user.php
@@ -8,9 +8,15 @@
 <?php 
 class User
 {
-	public username;
-	public password;
+	public $username;
+	public $password;
+	public static $passwordLength = 5;
+	
+	public static function getPasswordLength() {
+		return self::$passwordLength;
+	}
 }
+echo User::getPasswordLength(); // 5
 ?>
 </body>
 </html>
