bb3796380a4934bc8db8cb73773cb770ab7a8085
milivojevic85 <misagrf@yahoo.com>
2020-01-24 19:28:07 +0100

    user.php created for extra

---
+++ user.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>User</title>
+</head>
+<body>
+<?php 
+class User
+{
+	public username;
+	public password;
+}
+?>
+</body>
+</html>
